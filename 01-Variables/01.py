1d50fb6128b85841f6c2947e2ece3c5daca723ec
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-02-22 19:24:12 +0100

    end-task-01-variables

---
+++ 01-Variables/01.py
@@ -0,0 +1,37 @@
+1- x = False , x = True
+2- y = 5, y = 10
+3- z = 5.2, z = 3.4
+4- s = 'Mohammad Ghnim', s = "Mohammad Ghnim"
+5- to convert the int variablr to float we add an integer inside of the parentheses
+    y = 5 --> print(float(y))
+    output = 5.0
+6- to convert string to int data type:
+   print(int("8"))
+7- list of number from 1 to 5:
+   m = [1 , 2 , 3 , 4 ,5]
+8- tuple from 10 to 15:
+    n = (10 , 11 , 12 , 13 , 14 , 15)
+9- convert the list to tuple:
+    for example
+    l = [1, 2, 3, 4, 5]
+    t = tuple(l)
+    print(l)
+10- create a dict of three values:
+    d = {1 : 'mohammad' , 2 : 'wesam' , 3 : 'zakria'}
+11- yes
+    It allows us to write multiple statements on the same line.
+12- python is iterpreted
+13-
+    1- low level:
+        a) the language of the computer
+        b) consists of 1,0
+        c) easy to be excuted
+        d) hard to program
+        e) can run only of one kind of computer and have to be rewritten to another
+        f) like C, Assembly
+     2- high level:
+         easy to programm
+         takes less time to be coded
+         easy to be corrected
+         portable (only one program can run on any pc)
+         like java, c# and python
