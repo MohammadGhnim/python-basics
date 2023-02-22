3c8cc5da5186462dbeaee026234a60e19c5c0a5f
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-02-22 20:30:15 +0100

    end-task-operators1

---
+++ 02-Operators/02.py
@@ -0,0 +1,34 @@
+python operators tasks:
+1- what is the differences between =, ==
+   a) =: simple Assignment operator. It assigns values from right side operands to the left side operands
+        example: a = 10, the value 10 will be stored in the integer variable a
+   b) ==: check if the value of two operands is equal or not, if yes then it is True
+         example: if(a==10), this will check if the value stored in variable a is equal to 10. it will return True
+
+2- what do we mean by using !=
+   != : check if the value of two operands is not equal, if not then it is true
+3- what is the operator precedence
+   Arithmetic operators take precedence over logical operators. Python will always evaluate the arithmetic operators first (** is highest, then multiplication/division, then addition/subtraction). Next comes the relational operators. Finally, the logical operators are done last.
+4- create a variable x with value of 10
+   x = 10
+5- Increase x value by 15 by using assignment operator
+   x += 5    ---> 15
+6- Divide the x value by 5 using assignment operators
+   x /= 5    ---> 3.0
+7- Multiply the x value by 10 using assignment operator
+   x *= 10   ---> 30.0
+8- Get module of x on 3 using assignment operators
+   x %= 3    ---> 0.0
+9- Using python print the module of 11 to 4
+   print(11%4)   ---> 3
+10- Print the exponent of 2,3
+    print(2**3)   ---> 8
+11- Divide 11 by 3 using python division
+    print(11/3)   ---> 3.6666666666666665
+12- Can we divide 11 by 3 and get an integer number ?
+    yes, we use Floor Division to become a integer number
+    example:
+    print(11//3)   ---> 3
+
+
+---------------------------------------------------------------------------------
