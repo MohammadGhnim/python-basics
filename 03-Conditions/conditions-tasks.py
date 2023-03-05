d977110c7596bc04af2efe187f9e0341f42f31c6
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-05 01:04:12 +0100

    13-Q

--- 03-Conditions/conditions-tasks.py
+++ 03-Conditions/conditions-tasks.py
@@ -1,3 +1,59 @@
-Python Conditions Tasks
+# Python Conditions Tasks
 
-1- Check if 10 is bigger than 15 or not
+# 1- Check if 10 is bigger than 15 or not
+   x = 10
+   x > 15
+#--->( false )
+
+# 2- If 10 is not bigger than 15 print x is smaller than 15
+   if x > 15:
+      print('x is bigger than 15')
+   else:
+      print('x is smaller than 15')
+#---> (x is smaller than 15)
+
+# 3- In which cases we will use all
+
+  # we use all with more than one condition and all condition have to be right
+# 4- What is the differences between all , and
+
+   # The difference is the syntax
+
+   
+# 5- What is the differences between any , or
+
+   # The difference is the syntax
+# 6- If we need all the conditions to be true we will use ...
+
+   # all , and
+# 7- What is the differences between if , elif
+
+   # Multiple if's means your code would go and check all the if conditions, where as in case of elif, if one if condition satisfies it would not check other conditions
+# 8- What is the differences between elif else
+
+   # if we have another condition we can use elif with this condition , and python provides you
+   #with else as a default option in case no condition matches , else code will be executed
+# 9- Can we use more than one elif
+
+   # yes
+# 10- Can we use more than one else
+
+   # no
+# 11- write s simple ternary operator
+
+   x = 10
+   print('x = 10') if x == 10 else print('x != 10')
+
+# 12- in elif , python will check all the conditions no matter what ?
+
+   # python will check the first right condition
+
+#13- in elif we use else for ... ?
+
+    #  else is s default option in case no condition matches, else condition will be executed
+
+# 14- if we have this list [2,4,6,8,10] :
+     #1. check to see if 4 in this list or not
+     #2. check to see if 4 and 6 in this list on not
+     #3. check to see if 3 or 6 in this list
+     #4. check to see if 2 , 4 and 5 in this list Python Conditions Tasks
