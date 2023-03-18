dab8b3119e2ad094b8c9261da772c464f11aba73
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-18 22:53:31 +0100

    add-data-types-task

---
+++ 06-Data types/data-types.py
@@ -0,0 +1,33 @@
+#Python Data Types Tasks
+
+#1- Wich is the differences between ‘ ’ “ ” ‘’’ ‘’’
+
+'''
+a- Single & Double Quotes
+Enclose strings. Either single or double quotes are fine.
+Use single quotes as enclosing quotes to eliminate the need of escaping double quotes in a string, and vice versa.
+b- Triple Quotes
+Enclose strings containing both single and double quotes such that no escaping is needed.
+Enclose multi-line strings.
+'''
+#2- Create a string with the name ‘mystro’
+print('------------------------------------')
+
+name = 'mystro'
+#3- Make the string letters capital
+print('------------------------------------')
+
+
+name = 'mystro'
+print(name.upper())
+
+#4- Create a list of values from 10 to 20
+#5- Add 30 to the end of the list
+#6- Add 40 as the second element of the list
+#7- Print how many elements in the list
+#8- replace the second element in the list with 100
+#9- Create a tuple with values from 1 to 5
+#10- Can we add 10 to the end of the tuple?
+#11- Create a dict of value Mahmoud:28 , ahmed:30
+#12- Print Mahmoud age from the dict
+#13- What is the differences between mutable and immutable data types ?
